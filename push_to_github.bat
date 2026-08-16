1265c5437a78ab49d0fbc1255902d4ec5153db3d
shobhitbajpay455-pr <shobhitbajpay455@gmail.com>
2026-08-16 18:52:41 +0530

    Add 1-click push_to_github.bat script

---
+++ push_to_github.bat
@@ -0,0 +1,12 @@
+@echo off
+title Push Expense Management System to GitHub
+echo ===================================================
+echo   Pushing latest changes to GitHub...
+echo ===================================================
+echo.
+git push origin master
+echo.
+echo ===================================================
+echo   Done! Press any key to close this window.
+echo ===================================================
+pause
